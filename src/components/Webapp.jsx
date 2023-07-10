1482713dc7f7bf3f69f05e96d9b6db8ea4bd71d7
PranshuPandey90 <138874525+PranshuPandey90@users.noreply.github.com>
2023-07-10 11:43:46 +0530

    Update Webapp.jsx

--- src/components/Webapp.jsx
+++ src/components/Webapp.jsx
@@ -1,26 +1,27 @@
 import React, { useRef, useEffect, useState } from "react";
 import { videoArr } from "../common/common.js";
 import "./style.css";
+import "video.js/dist/video-js.css";
 
 const Webapp = () => {
-  const videoRefs = useRef([]);
-  const [currentVideo, setCurrentVideo] = useState();
+  const videoRef = useRef([]);
+  const [selectedVideo, setSelectedVideo] = useState();
 
   useEffect(() => {
-    if (currentVideo) {
-      currentVideo.muted = true;
+    if (selectedVideo) {
+      selectedVideo.muted = false;
     }
-  }, [currentVideo]);
+  }, [selectedVideo]);
 
   useEffect(() => {
     const handleScroll = () => {
-      const videos = videoRefs.current;
+      const videos = videoRef.current;
 
       videos.forEach((video) => {
-        const rect = video.getBoundingClientRect();
+        const screen = video.getBoundingClientRect();
         const isVisible =
-          rect.top < window.innerHeight / 2 &&
-          rect.bottom >= window.innerHeight / 2;
+        screen.top < window.innerHeight / 2 &&
+        screen.bottom >= window.innerHeight / 2;
 
         if (isVisible) {
           video.play();
@@ -39,8 +40,8 @@ const Webapp = () => {
   }, []);
 
   const handleVideoClick = (index) => {
-    const clickedVideo = videoRefs.current[index];
-    setCurrentVideo(clickedVideo);
+    const clickedVideo = videoRef.current[index];
+    setSelectedVideo(clickedVideo);
     if (clickedVideo.paused) {
       clickedVideo.play();
     } else {
@@ -50,18 +51,19 @@ const Webapp = () => {
 
   return (
     <div className="container">
-      {videoArr.map((arr, index) => {
-        const recommendation = arr.data.recommendation;
+      {videoArr.map((vid, index) => {
+        const recommendation = vid.data.recommendation;
         return recommendation.map((video, index) => (
           <div
-            className="video_wrapper  y mandatory-scroll-snapping"
+            className="wrap-video  y mandatory-scroll-snapping"
             key={video.video_url.med + index}
             dir="ltr"
           >
-            <video loop
-              className="video_container"
+            <video
+              loop
+              className="inner-container"
               key={video.video_url.med}
-              ref={(ref) => (videoRefs.current[index] = ref)}
+              ref={(ref) => (videoRef.current[index] = ref)}
               autoPlay={index === 0 ? true : false}
               muted={true}
               onClick={() => handleVideoClick(index)}
